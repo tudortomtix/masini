baddf1bbe79e9df8b3c5b520b9b81ab129a70d17
Tudor Fratila <Tudor@MacBook-Air-Tudor.local>
2017-12-01 17:11:50 +0200

    Initial Commit

---
+++ masini/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  masini
+//
+//  Created by Tudor Fratila on 01/12/2017.
+//  Copyright © 2017 Tudor Fratila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
